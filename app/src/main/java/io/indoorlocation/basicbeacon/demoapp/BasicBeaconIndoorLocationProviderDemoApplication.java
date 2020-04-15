ea23a1aa746903d816c65b5b9eddfb0a1b620586
Etienne <etienne@mapwize.io>
2020-04-15 14:09:13 +0200

    Update demo app with MapwizeSDK 3.2.1 & MapwizeUI 2.2.0

--- app/src/main/java/io/indoorlocation/basicbeacon/demoapp/BasicBeaconIndoorLocationProviderDemoApplication.java
+++ app/src/main/java/io/indoorlocation/basicbeacon/demoapp/BasicBeaconIndoorLocationProviderDemoApplication.java
@@ -2,14 +2,19 @@ package io.indoorlocation.basicbeacon.demoapp;
 
 import android.app.Application;
 
-import io.mapwize.mapwizeformapbox.AccountManager;
+import com.mapbox.mapboxsdk.Mapbox;
+
+import io.mapwize.mapwizesdk.core.MapwizeConfiguration;
 
 public class BasicBeaconIndoorLocationProviderDemoApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        AccountManager.start(this, "1f04d780dc30b774c0c10f53e3c7d4ea");// PASTE YOU MAPWIZE API KEY HERE !!! This is a demo key, giving you access to the demo building. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signin up at mapwize.io
+        Mapbox.getInstance(this, "pk.mapwize");
+        // Mapwize globale initialization
+        MapwizeConfiguration config = new MapwizeConfiguration.Builder(this, "YOUR_API_KEY").build();
+        MapwizeConfiguration.start(config);
     }
 
 }
--- app/src/main/java/io/indoorlocation/basicbeacon/demoapp/MapActivity.java
+++ app/src/main/java/io/indoorlocation/basicbeacon/demoapp/MapActivity.java
@@ -2,45 +2,100 @@ package io.indoorlocation.basicbeacon.demoapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+import java.util.Locale;
+
 import io.indoorlocation.basicbeaconlocationprovider.BasicBeaconIndoorLocationProvider;
 import io.indoorlocation.gps.GPSIndoorLocationProvider;
-import io.mapwize.mapwizeformapbox.MapOptions;
-import io.mapwize.mapwizeformapbox.MapwizePlugin;
+import io.mapwize.mapwizesdk.api.MapwizeObject;
+import io.mapwize.mapwizesdk.map.MapOptions;
+import io.mapwize.mapwizesdk.map.MapwizeMap;
+import io.mapwize.mapwizesdk.map.MapwizeView;
+import io.mapwize.mapwizeui.MapwizeFragment;
+import io.mapwize.mapwizeui.MapwizeFragmentUISettings;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements MapwizeFragment.OnFragmentInteractionListener {
 
-    private MapView mapView;
-    private MapwizePlugin mapwizePlugin;
+    private MapwizeFragment mapwizeFragment;
     private BasicBeaconIndoorLocationProvider basicBeaconIndoorLocationProvider;
     private GPSIndoorLocationProvider gpsIndoorLocationProvider;
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 0;
 
+    MapwizeView mapwizeView;
+    MapwizeMap mapwizeMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, "pk.eyJ1IjoibWFwd2l6ZSIsImEiOiJjamNhYnN6MjAwNW5pMnZvMnYzYTFpcWVxIn0.veTCqUipGXCw8NwM2ep1Xg");// PASTE YOU MAPBOX API KEY HERE !!! This is a demo key. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signing up at mapbox.com
         setContentView(R.layout.activity_map);
 
-        mapView = findViewById(R.id.mapview);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                mapwizePlugin = new MapwizePlugin(mapView, mapboxMap, new MapOptions());
-                startLocationService();
-            }
-        });
+        FrameLayout container = findViewById(R.id.container);
+
+        MapOptions opts = new MapOptions.Builder()
+                .language(Locale.getDefault().getLanguage()).build();
+        mapwizeFragment = MapwizeFragment.newInstance(opts);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(container.getId(), mapwizeFragment);
+        ft.commit();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapwizeFragment.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapwizeFragment.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapwizeFragment.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapwizeFragment.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        mapwizeFragment.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapwizeFragment.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapwizeFragment.onDestroy();
+        super.onDestroy();
     }
 
     private void startLocationService() {
@@ -55,7 +110,7 @@ public class MapActivity extends AppCompatActivity {
     private void setupLocationProvider() {
         gpsIndoorLocationProvider = new GPSIndoorLocationProvider(this);
         basicBeaconIndoorLocationProvider = new BasicBeaconIndoorLocationProvider(this, "1f04d780dc30b774c0c10f53e3c7d4ea", gpsIndoorLocationProvider);
-        mapwizePlugin.setLocationProvider(basicBeaconIndoorLocationProvider);
+        mapwizeMap.setIndoorLocationProvider(basicBeaconIndoorLocationProvider);
     }
 
     @Override
@@ -64,53 +119,30 @@ public class MapActivity extends AppCompatActivity {
             case MY_PERMISSION_ACCESS_FINE_LOCATION: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     setupLocationProvider();
-
                 }
             }
         }
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
+    public void onMenuButtonClick() {
+        Log.i("MapActivity", "onMenuButtonClick");
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public void onInformationButtonClick(MapwizeObject mapwizeObject) {
+        Log.i("MapActivity", "onInformationButtonClick");
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
+    public void onFragmentReady(MapwizeMap mapwizeMap) {
+        this.mapwizeMap = mapwizeMap;
+        startLocationService();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+    public void onFollowUserButtonClickWithoutLocation() {
+        Log.i("MapActivity", "onFollowUserButtonClickWithoutLocation");
     }
 }
